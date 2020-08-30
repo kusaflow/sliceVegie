386c0c8a5a23c885d35e34463f220968b4594176
Kunal SAgar <kunalsagar1999@gmail.com>
2020-08-30 22:34:43 +0530

    init

---
+++ Source/SliceVegie/SliceVegieGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SliceVegieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLICEVEGIE_API ASliceVegieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
